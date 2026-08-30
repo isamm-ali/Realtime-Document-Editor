b4d6a9021bb60219dc1ce5928e11f0059161a221
Isam Ali <isamali.work@gmail.com>
2026-08-30 12:55:59 +0530

    .

--- frontend/lib/repositories/local_storage_repository.dart
+++ frontend/lib/repositories/local_storage_repository.dart
@@ -3,12 +3,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 class LocalStorageRepository {
   Future<void> setToken(String token) async {
     SharedPreferences preferences = await SharedPreferences.getInstance();
-    preferences.setString('auth-token', token);
+    preferences.setString('token', token);
   }
 
   Future<String?> getToken() async {
     SharedPreferences preferences = await SharedPreferences.getInstance();
-    String? token = preferences.getString('auth-token');
+    String? token = preferences.getString('token');
     return token;
   }
 }
